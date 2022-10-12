1ec241c49667b3b5b9403c5a0583471d8715345a
curtisblanchette <hello@curtisblanchette.com>
2022-10-12 15:02:27 -0700

    fix(master): public/private subnet route table configurations

--- vpc/main.tf
+++ vpc/main.tf
@@ -86,39 +86,46 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route" "public" {
+resource "aws_route" "public_igw" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
 
-#resource "aws_route_table" "private" {
-#  count  = length(var.private_subnets)
-#  vpc_id = aws_vpc.main.id
-#
-#  tags = {
-#    Name        = "${var.name}-routing-table-private-${format("%03d", count.index+1)}"
-#    Environment = var.environment
-#  }
-#}
+resource "aws_route" "public_nat" {
+  count                  = length(compact(var.public_subnets))
 
-#resource "aws_route" "private" {
-#  count                  = length(compact(var.private_subnets))
-#  route_table_id         = element(aws_route_table.private.*.id, count.index)
-#  destination_cidr_block = "0.0.0.0/0"
-#  nat_gateway_id         = element(aws_nat_gateway.main.*.id, count.index)
-#}
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = element(var.public_subnets, count.index)
+  nat_gateway_id         = aws_nat_gateway.main[count.index].id
+}
 
-#resource "aws_route_table_association" "private" {
-#  count          = length(var.private_subnets)
-#  subnet_id      = element(aws_subnet.private.*.id, count.index)
-#  route_table_id = element(aws_route_table.private.*.id, count.index)
-#}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "${var.name}-routing-table-private-${var.environment}"
+    Environment = var.environment
+  }
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.main[0].id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = aws_route_table.private.id
+}
 
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnets)
   subnet_id      = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.public.id
+  route_table_id = element(aws_route_table.public.*.id, count.index)
 }
 
 resource "aws_flow_log" "main" {
