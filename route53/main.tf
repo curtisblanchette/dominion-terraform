afb9e90e27b7908d73113b09b0a2f1205dec9153
Curt Blanchette <6607883+curtisblanchette@users.noreply.github.com>
2022-12-30 10:26:41 -0800

    fix(master): update cognito sns_region and fqdn

--- route53/main.tf
+++ route53/main.tf
@@ -1,6 +1,14 @@
+resource "aws_route53domains_registered_domain" "main" {
+  domain_name = "curtisblanchette.com"
+}
+
+data "aws_route53_zone" "main" {
+  name = "curtisblanchette.com"
+}
+
 resource "aws_route53_record" "alb_subdomain" {
-  zone_id = var.hosted_zone_id
-  name    = "dominion-alb-${var.environment}.4iiz.io"
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "dominion-alb-${var.environment}.curtisblanchette.com"
   type    = "A"
 
   alias {
@@ -10,27 +18,27 @@ resource "aws_route53_record" "alb_subdomain" {
   }
 }
 
-resource "aws_route53_record" "gateway_alias" {
-  count = terraform.workspace == "default" ? 1 : 0
-  zone_id = var.hosted_zone_id
-  name    = "dominion-api.4iiz.io"
-  type    = "A"
-
-  alias {
-    name                   = var.deployment_invoke_url
-    zone_id                = "Z1UJRXOUMOOFQ8" # https://docs.aws.amazon.com/general/latest/gr/apigateway.html
-    evaluate_target_health = true
-  }
-}
+#resource "aws_route53_record" "gateway_alias" {
+#  count = terraform.workspace == "default" ? 1 : 0
+#  zone_id = aws_route53_zone.main.zone_id
+#  name    = "dominion-api.curtisblanchette.com"
+#  type    = "A"
+#
+#  alias {
+#    name                   = var.deployment_invoke_url
+#    zone_id                = "Z1UJRXOUMOOFQ8" # https://docs.aws.amazon.com/general/latest/gr/apigateway.html
+#    evaluate_target_health = true
+#  }
+#}
 
 resource "aws_route53_record" "ui_subdomain" {
-  zone_id = "Z1U9E1P8BYPT7O"
-  name    = "app2-${var.environment}.4iiz.io"
+  zone_id = "Z0521237I3C05XVWU5AR"
+  name    = "app-${var.environment}.curtisblanchette.com"
   type    = "A"
 
   alias {
     name = var.cloudfront_domain_name
-    zone_id = "Z2FDTNDATAQYW2" # CloudFront ZoneID (https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-route53-aliastarget.html)
+    zone_id = "Z2FDTNDATAQYW2" # This is always the CloudFront ZoneID (https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-route53-aliastarget.html)
     evaluate_target_health = false
   }
 }
@@ -42,4 +50,8 @@ output "route53_alb_record_uri" {
 
 output "route53_ui_record_uri" {
   value = aws_route53_record.ui_subdomain.records
+}
+
+output "route53_hosted_zone_id" {
+  value = data.aws_route53_zone.main.zone_id
 }
--- route53/variables.tf
+++ route53/variables.tf
@@ -5,30 +5,30 @@ variable "name" {
 variable "environment" {
   description = "the name of your environment, e.g. \"prod\""
 }
-
-variable "hosted_zone_id" {
-  description = "the id of the hosted zone for dns."
-}
+#
+#variable "hosted_zone_id" {
+#  description = "the id of the hosted zone for dns."
+#}
 
 variable "aws_alb_dns_name" {
   description = "then dns name of your alb."
 }
 
-variable "route53_alb_record_uri" {
-  description = "the cname of the alb"
-}
-
-variable "route53_ui_record_uri" {
-  description = "the cname of the ui"
-}
+#variable "route53_alb_record_uri" {
+#  description = "the cname of the alb"
+#}
+#
+#variable "route53_ui_record_uri" {
+#  description = "the cname of the ui"
+#}
 
 variable "aws_alb_zone_id" {
   description = "the hosted zone id for dns"
 }
 
-variable "deployment_invoke_url" {
-  description = "the api gateway stage invoke url"
-}
+#variable "deployment_invoke_url" {
+#  description = "the api gateway stage invoke url"
+#}
 
 variable "cloudfront_domain_name" {
   description = "the domain name of the cloudfront distribution"
